a6cda47fcf33fc3b9ce9da2bf1463339b1a4a820
MMoon <108307939+MMoonDzn@users.noreply.github.com>
2025-04-04 18:40:14 -0300

    Update loaderS.lua

--- loaderS.lua
+++ loaderS.lua
@@ -1,6 +1,6 @@
 local Games = {
     CBOH = {
-        Ids = {"107755520266965", "114948338750036", "100085170632745"},
+        Ids = {"116678050278196", "88599421783389", "134260825047511"},
         SourceUrl = "https://api.luarmor.net/files/v3/loaders/d40f51990986aaaf594c595a85f50f38.lua"
     }
 }
