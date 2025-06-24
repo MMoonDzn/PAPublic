40be157ef28108ca82c13c991d749022819a58d2
MMoon <108307939+MMoonDzn@users.noreply.github.com>
2025-06-24 13:25:44 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -22,6 +22,10 @@ local PlaceId = tostring(game.PlaceId)
 local Game, SourceUrl = LocateGame(PlaceId)
 
 if Game ~= "Unknown" and SourceUrl then
+    if Game == "TSV4" then
+        warn(("[Aurora] Detected game: %s. Loading script..."):format(Game))
+        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/0e59e9e97e9535a9d72fa78537c36cb7.lua"))()
+    else
         warn(("[Aurora] Detected game: %s. Loading script..."):format(Game))
         loadstring(game:HttpGet(SourceUrl))()
     end
